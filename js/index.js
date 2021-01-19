bf19c998ca201f862a84705076a11978b3770878
Cindy <cindytvn+git@gmail.com>
2021-01-19 22:49:28 +0100

    finished till it. 2 incl using the getElementsByClassName! Tomorrow early I finish the rest

--- js/index.js
+++ js/index.js
@@ -3,18 +3,48 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  // get value from price:
+  const price = product.querySelector('.price span')
+  let priceValue = price.innerText
+  //console.log('value price: ', priceValue, typeof priceValue)
+
+  // get value from input form (using key 'value')
+  let quantity = product.querySelector('#quantity-input')
+  let quantityValue = quantity.value
+ //console.log('elem quantity', quantityValue,  typeof quantityValue)
+
+  // new var equal to the product of the values
+  let subtotal = product.querySelector('.subtotal span') //make object
+  let subtotalValue = subtotal.innerText // get value
+  subtotalValue = priceValue * quantityValue // give it the total value
+  subtotal.innerText = subtotalValue // put the subt value to the element
+
+  // console.log(typeof subtotalValue)
+  // console.log('total price ', typeof subtotalValue, subtotalValue)
+
+  return(subtotalValue)
+
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+ 
+  let allProd = document.getElementsByClassName('product')
+
+  //let allProdValue = 
+  // console.log(allProd.innerText)
+  // console.log(allProd, typeof allProd)
+  // console.log(allProd[1])
+
+for (let i of allProd) {
+  updateSubtotal(i)
+}
+  // //---test ---
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  
 
   // ITERATION 3
   //... your code goes here
